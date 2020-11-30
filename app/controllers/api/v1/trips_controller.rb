e739173285f7b69e3d4da1a1f308109ef6e995ee
Erica Oh <oh.erica19@gmail.com>
2020-11-30 15:52:43 +0800

    added users and trips models and controllers

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::TripsController < Api::V1::BaseController
+
+skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+
+  def index
+      @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.save
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.update(trip_params)
+    render json: @trip
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    render json: @trip
+    @trip.destroy
+  end
+
+  private
+  def trip_params
+    params.require(:trips).permit(:location, :start_date, :end_date)
+  end
+end
