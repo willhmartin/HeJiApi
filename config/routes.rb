cec9babc563bd69768c13d44b5c1d7309eaaba75
Erica Oh <oh.erica19@gmail.com>
2020-12-01 17:12:29 +0800

    finished api requests/routes for activities

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [ :show, :create ] do
         resources :trips, only: [ :index, :show, :create, :update, :destroy ]
+        end
+
+
+      resources :trips, only: [ :index, :show, :create, :update, :destroy ] do
+        resources :activities, only: [ :index, :show, :create, :update, :destroy ]
       end
 
-      resources :activities, only: [ :index, :show, :create, :update, :destroy ]
       resources :guests, only: [ :show, :create ]
       resources :payments, only: [ :index, :create, :update, :destroy ]
       resources :budgets, only: [ :show, :create, :update, :destroy ]
