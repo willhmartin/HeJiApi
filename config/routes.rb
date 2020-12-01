6386487362c9a276373313a3a8bd41d47836bdc1
Erica Oh <oh.erica19@gmail.com>
2020-12-01 15:33:52 +0800

    seeded users and trips for user and trip routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :show, :create ]
-      resources :trips, only: [ :index, :show, :create, :update, :destroy ]
+      resources :users, only: [ :show, :create ] do
+        resources :trips, only: [ :index, :show, :create, :update, :destroy ]
+      end
+
       resources :activities, only: [ :index, :show, :create, :update, :destroy ]
       resources :guests, only: [ :show, :create ]
       resources :payments, only: [ :index, :create, :update, :destroy ]
