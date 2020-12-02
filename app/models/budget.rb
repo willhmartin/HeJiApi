376a85f7bde4c73d3bccfe683dc3a7d009c76d02
Will Martin <whmartinwork@outlook.com>
2020-12-02 13:26:24 +0800

    budget typo

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,6 +2,6 @@ class Budget < ApplicationRecord
   validates :amount, presence: true
   belongs_to :user
   belongs_to :trip
-  has_many :payments, through :trip
+  has_many :payments, through: :trip
 end
 
