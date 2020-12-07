236f3519b6b0c5ff066a3dd65f033bf62b07c49a
Will Martin <whmartinwork@outlook.com>
2020-12-07 11:46:52 +0800

    fixed login backend

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,15 +4,15 @@ URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
 
 
   def wechat_user
-    wechat_params = {
+    @wechat_params = {
       appId: ENV["WECHAT_APP_ID"],
       secret: ENV["WECHAT_APP_SECRET"],
       js_code: params[:code],
       grant_type: "authorization_code"
     }
-
-    @wechat_response ||= RestClient.get(URL, params: wechat_params)
+    @wechat_response ||= RestClient.get(URL, params: @wechat_params)
     @wechat_user ||= JSON.parse(@wechat_response.body)
+
   end
 
   def login
